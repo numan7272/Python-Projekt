aa7deffe828ef3a41add9fade33fd04b47c35eab
BuildTools <unconfigured@null.spigotmc.org>
2025-08-31 03:43:32 +0200

    modified:   probieren.py

--- probieren.py
+++ probieren.py
@@ -1,9 +1,13 @@
 import time
 import random
 
-text = "Bababoiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"
+#2 5 4 3 1
 
-for abc in text:
-    print(abc, end="\r", flush=True)
-    time.sleep(random.uniform(0.05, 0.2))
-    
+Eingabe = list(input("Gib deine gewünschte sortierte Zahlenliste an: ").split())
+
+list.sort(Eingabe)
+
+text = f"Hier ist deine sortierte Liste{Eingabe}"
+for platz1 in text:
+    print(platz1, end="", flush=True)
+    time.sleep(random.uniform(0.1, 0.15))
