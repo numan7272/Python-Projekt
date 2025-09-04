87227ef28d73e72e813a14beda768ae946b4018b
BuildTools <unconfigured@null.spigotmc.org>
2025-09-04 21:18:19 +0200

    modified:   "src/Galgenm\303\244nnchen/w\303\266rterliste.py"

--- src/Galgenmännchen/wörterliste.py
+++ src/Galgenmännchen/wörterliste.py
@@ -2,26 +2,26 @@
 
 Wörter = (
     # Früchte
-    "Apfel", "Banane", "Kirsche", "Orange", "Birne", "Pfirsich", "Traube", "Melone", "Erdbeere", "Ananas",
-    "Mango", "Kiwi", "Zitrone", "Limette", "Mandarine", "Granatapfel", "Feige", "Dattel", "Kokosnuss", "Papaya",
+"apfel", "banane", "kirsche", "orange", "birne", "pfirsich", "traube", "melone", "erdbeere", "ananas",
+"mango", "kiwi", "zitrone", "limette", "mandarine", "granatapfel", "feige", "dattel", "kokosnuss", "papaya",
 
-    # Gemüse
-    "Karotte", "Tomate", "Gurke", "Paprika", "Zwiebel", "Brokkoli", "Spinat", "Kartoffel", "Blumenkohl", "Zucchini",
-    "Lauch", "Sellerie", "Kürbis", "Rote Beete", "Fenchel", "Erbse", "Bohne", "Mais", "Artischocke", "Chili",
+# Gemüse
+"karotte", "tomate", "gurke", "paprika", "zwiebel", "brokkoli", "spinat", "kartoffel", "blumenkohl", "zucchini",
+"lauch", "sellerie", "kürbis", "rote beete", "fenchel", "erbse", "bohne", "mais", "artischocke", "chili",
 
-    # Tiere
-    "Hund", "Katze", "Maus", "Vogel", "Elefant", "Löwe", "Tiger", "Affe", "Bär", "Fisch",
-    "Pferd", "Esel", "Schaf", "Kuh", "Ziege", "Huhn", "Gans", "Ente", "Adler", "Fuchs",
+# Tiere
+"hund", "katze", "maus", "vogel", "elefant", "löwe", "tiger", "affe", "bär", "fisch",
+"pferd", "esel", "schaf", "kuh", "ziege", "huhn", "gans", "ente", "adler", "fuchs",
 
-    # Farben
-    "Rot", "Blau", "Grün", "Gelb", "Orange", "Lila", "Schwarz", "Weiß", "Braun", "Grau",
-    "Pink", "Türkis", "Beige", "Silber", "Gold",
+# Farben
+"rot", "blau", "grün", "gelb", "orange", "lila", "schwarz", "weiß", "braun", "grau",
+"pink", "türkis", "beige", "silber", "gold",
 
-    # Länder
-    "Deutschland", "Frankreich", "Italien", "Spanien", "Portugal", "Niederlande", "Österreich", "Schweiz", "Polen", "Schweden",
-    "Norwegen", "Dänemark", "Finnland", "Griechenland", "Türkei", "Ägypten", "Marokko", "China", "Japan", "Indien",
+# Länder
+"deutschland", "frankreich", "italien", "spanien", "portugal", "niederlande", "österreich", "schweiz", "polen", "schweden",
+"norwegen", "dänemark", "finnland", "griechenland", "türkei", "ägypten", "marokko", "china", "japan", "indien",
 
-    # Zufällige Begriffe
-    "Computer", "Programm", "Tisch", "Stuhl", "Fenster", "Lampe", "Auto", "Zug", "Schiff", "Flugzeug",
-    "Haus", "Garten", "Straße", "Brücke", "Buch", "Zeitung", "Stift", "Papier", "Uhr", "Telefon"
+# Zufällige Begriffe
+"computer", "programm", "tisch", "stuhl", "fenster", "lampe", "auto", "zug", "schiff", "flugzeug",
+"haus", "garten", "straße", "brücke", "buch", "zeitung", "stift", "papier", "uhr", "telefon"
 )
